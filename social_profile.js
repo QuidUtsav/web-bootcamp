b2dc46831a4ebd847bfcf9f16b4c579cf1f30a8c
QuidUtsav <pas081bct048@wrc.edu.np>
2026-07-26 11:04:42 +0545

    completed assignment

--- social_profile.js
+++ social_profile.js
@@ -1,3 +1,5 @@
+const { log } = require("node:console");
+
 const profile ={
     "fullName": "Utsav Thapa",
     "email": "utsav@gmail.com",
@@ -6,3 +8,59 @@ const profile ={
     "bookmarks": false,
     "friendsName": ["xyz", "abc"]
 }
+
+function  like(){
+    profile.numberOfLike++;
+}
+like();
+
+function editProfile(fullname, email){
+    profile.fullName = fullname;
+    profile.email = email;
+}
+editProfile("hari","h@.com");
+
+function editComments(operation,item){
+    if (operation === "add"){
+        profile.comments.push(item)
+    }
+    else if(operation==="remove"){
+        profile.comments.filter((comment)=>{
+        profile.comments = profile.comments.filter((comment) => comment !== item);
+    })
+    }
+    else{
+        return "error"
+    }
+    return "operation done."
+}
+function editFriends(operation,item){
+    if (operation === "add"){
+        profile.friendsName.push(item)
+    }
+    else if(operation==="remove"){
+        profile.friendsName.filter((comment)=>{
+        profile.friendsName = profile.friendsName.filter((comment) => comment !== item);
+    })
+    }
+    else{
+        return "error"
+    }
+    return "operation done."
+}
+
+function toggleBookmark(){
+    if(profile.bookmarks){
+        profile.bookmarks=false;
+    }else{
+        profile.bookmarks=true;
+    }
+}
+toggleBookmark();
+toggleBookmark();
+toggleBookmark();
+toggleBookmark();
+toggleBookmark();
+editComments("remove","y")
+editFriends("add","ramesh")
+console.log(profile);
